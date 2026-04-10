4cc4d5a1182afa8d6ad3bc419cfd9efa93b769c6
Andrey Svistunov <andrey@svistunov.online>
2026-04-10 18:41:05 +0300

    nativesql: create sub-package with design spec reference

---
+++ mondrian/src/main/java/mondrian/rolap/nativesql/package-info.java
@@ -0,0 +1,16 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2026 Hitachi Vantara and others
+// All Rights Reserved.
+*/
+/**
+ * Cell-phase native SQL work registry + shared substrate primitives.
+ *
+ * See docs/superpowers/specs/2026-04-10-cell-phase-native-registry-design.md
+ * for the design contracts governing all types in this package.
+ */
+package mondrian.rolap.nativesql;
